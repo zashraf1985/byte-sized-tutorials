8cc58b78622757bd6967c49780438b8ef86ec9cc
zashraf1985 <zeeshanashraf1985@gmail.com>
2024-10-25 23:57:35 -0700

    added github action tutorial code

---
+++ ci-cd/02-github-action-ci/test_app.py
@@ -0,0 +1,9 @@
+import unittest
+from app import hello
+
+class TestApp(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello, World!")
+
+if __name__ == "__main__":
+    unittest.main()
